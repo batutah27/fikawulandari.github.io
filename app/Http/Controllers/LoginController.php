5bd8c34d3f591041fa0c411618f3c0c49f49d58a
batutah27 <140912959+batutah27@users.noreply.github.com>
2024-07-11 17:29:47 +0700

    Add files via upload

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function postlogin(Request $request)
+    {
+        if (Auth::attempt($request->only('email', 'password'))) {
+            return redirect('dashboard');
+        }
+        return redirect('login');
+    }
+}
